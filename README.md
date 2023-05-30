8246581fbc77911be9c34a8a94a0b864af57d8e6
linda423udll <46473881+linda423udll@users.noreply.github.com>
2023-05-30 14:47:54 +0800

    delete project
    delete project

--- README.md
+++
@@ -1,2 +0,0 @@
-# OrderFoodSystem
-EAT heathy 
--- docAPI/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"com.SB","l":"A"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.SB","l":"B"},{"p":"com.SA","l":"student1"},{"p":"com.SA","l":"student2"}];updateSearchResults();
